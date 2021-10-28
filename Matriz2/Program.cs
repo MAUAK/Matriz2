4a95492b6e3fb398b13bc78cce244434cd9204c8
MAUAK <kauamsilva360@gmail.com>
2021-10-27 21:38:37 -0300

    att

--- Matriz2/Program.cs
+++ Matriz2/Program.cs
@@ -40,28 +40,72 @@ namespace Matriz2
             for (int i = 0; i < n; i++)
             {
                 //Criando a repetição com o número de colunas menor q a variável N
-                for (int j = 0; j < n; j++)
+                for (int j = 0; j < m; j++)
                 {
                     //Comparando se o valor da posição da linha e da coluna é menor que zero
                     if (mat[i, j] == x)
                     {
-                        //Criando váriaveis para saber qual o número antes, depois, em cima e em baixo do número escolhido
-                        int esquerdo = mat[i, j - 1];
-                        int direito = mat[i, j + 1];
-                        int acima = mat[i - 1, j];
-                        int abaixo = mat[i + 1, j];
-
-                        /*if (esquerdo == null || direito == null || acima == null || abaixo == null)
+                        if (i > 0 && j > 0 && i != (n - 1) && j != (m - 1))
                         {
+                            //Criando váriaveis para saber qual o número antes, depois, em cima e em baixo do número escolhido
+                            int esquerdo = mat[i, j - 1];
+                            int direito = mat[i, j + 1];
+                            int acima = mat[i - 1, j];
+                            int abaixo = mat[i + 1, j];
 
-                        }*/
+                            //Imprimindo cada número
+                            Console.WriteLine("\n-------------------------------");
+                            Console.WriteLine("Posição: " + i + "," + j);
+                            Console.WriteLine("Número acima: " + acima);
+                            Console.WriteLine("Número abaixo: " + abaixo);
+                            Console.WriteLine("Número a direita: " + direito);
+                            Console.WriteLine("Número a esquerda: " + esquerdo);
+                            Console.WriteLine("-------------------------------");
+                        }
+                        else if (i == 0 && j ==0)
+                        {
+                            int direito = mat[i, j + 1];
+                            int abaixo = mat[i + 1, j];
 
-                        //Imprimindo cada número
-                        Console.WriteLine("\nNúmero acima: " + acima);
-                        Console.WriteLine("Número abaixo: " + abaixo);
-                        Console.WriteLine("Número a direita: " + direito);
-                        Console.WriteLine("Número a esquerda: " + esquerdo);
-                        Console.WriteLine("-------------------------------");
+                            Console.WriteLine("\n-------------------------------");
+                            Console.WriteLine("Posição: " + i + "," + j);
+                            Console.WriteLine("Número abaixo: " + abaixo);
+                            Console.WriteLine("Número a direita: " + direito);
+                            Console.WriteLine("-------------------------------");
+                        }
+                        else if (i == (n-1) && j == (m-1))
+                        {
+                            int esquerdo = mat[i, j - 1];
+                            int acima = mat[i - 1, j];
+
+                            Console.WriteLine("\n-------------------------------");
+                            Console.WriteLine("Posição: " + i + "," + j);
+                            Console.WriteLine("Número acima: " + acima);
+                            Console.WriteLine("Número a esquerda: " + esquerdo);
+                            Console.WriteLine("-------------------------------");
+                        }
+                        else if (i == (n-1) && j == 0)
+                        {
+                            int direito = mat[i, j + 1];
+                            int acima = mat[i - 1, j];
+
+                            Console.WriteLine("\n-------------------------------");
+                            Console.WriteLine("Posição: " + i + "," + j);
+                            Console.WriteLine("Número acima: " + acima);
+                            Console.WriteLine("Número a direita: " + direito);
+                            Console.WriteLine("-------------------------------");
+                        }
+                        else if (i == 0 && j == (m - 1))
+                        {
+                            int esquerdo = mat[i, j - 1];
+                            int abaixo = mat[i + 1, j];
+
+                            Console.WriteLine("\n-------------------------------");
+                            Console.WriteLine("Posição: " + i + "," + j);
+                            Console.WriteLine("Número a esquerda: " + esquerdo);
+                            Console.WriteLine("Número abaixo: " + abaixo);
+                            Console.WriteLine("-------------------------------");
+                        }               
                     }
                     /*else
                     {
@@ -69,66 +113,7 @@ namespace Matriz2
                         Console.WriteLine("\nEste número não existe.");
                     }*/
                 }
-            }
-
-
-            /*
-            //Criando a variavel
-            int n = int.Parse(Console.ReadLine());
-
-            //Criando a matriz com os valores de coluna e linha = n
-            int[,] mat = new int[n, n];
-
-            //Criando a repetição para adicionar os valores da matriz, colocando como parametro os valores de linhas menores que a variavel N
-            for (int i = 0; i < n; i++)
-            {
-                //Lendo cada número da matriz
-                string[] values = Console.ReadLine().Split(' ');
-
-                //Criando a repetição para adicionar os valores da matriz, colocando como parametro os valores de colunas menores que a variavel N
-                for (int j = 0; j < n; j++)
-                {
-                    //Lendo os valores para cada posição da matriz
-                    mat[i, j] = int.Parse(values[j]);
-                }
-            }
-
-            
-            //Imprimindo a informação da diagonal principal
-            Console.WriteLine("Main diagonal: ");
-
-            //Criando a repetição para pegar os valores da matriz principal
-            for (int i = 0; i < n; i++)
-            {
-                //Imprimindo os valores que tem a coluna e a linha como valores iguais
-                Console.Write(mat[i, i] + " ");
-            }
-            //Imprimindo uma linha em branco
-            Console.WriteLine();
-
-            //Criando variavel para contar os números negativos
-            int count = 0;
-
-            //Criando a repetição com o número de linhas menor q a variável N
-            for (int i = 0; i < n; i++)
-            {
-                //Criando a repetição com o número de colunas menor q a variável N
-                for (int j = 0; j < n; j++)
-                {
-                    //Comparando se o valor da posição da linha e da coluna é menor que zero
-                    if (mat[i, j] < 0)
-                    {
-                        //Somando a variavel count + 1
-                        count++;
-                    }
-                }
-            }
-            //Imprimindo quantos números negativos têm na matriz
-            Console.WriteLine("Negative number: " + count);
-
-            //--------------------------------------------------------------------
-
-            */
+            }            
         }
     }
 }
